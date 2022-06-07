7da4396e40f8d1175ad68ab3bf28dbba34a0616f
Jenna DiStefano <88553345+jdistefano3@users.noreply.github.com>
2022-06-07 14:25:08 -0400

    Create backward-digit-span.js

---
+++ Backward Digit Span/backward-digit-span.js
@@ -0,0 +1,30 @@
+var timeline = [];
+
+
+//Function to give the option for a local save of the data
+var localSave;
+
+function saveData() {
+	if(localSave == 1) {
+		var identifier = 'BDS_'+Math.round(new Date().getTime()/1000)+'.csv';
+		jsPsych.data.get().localSave('csv',identifier);
+		alert("You may now close this tab.");
+	} else {
+		alert("You may now close this tab.");
+		}
+	};
+
+//Final screen
+var save_data = {
+	type: 'html-button-response',
+	stimulus: '<p>This concludes the task. Would you like to save the data?</p>',
+	choices: ['No','Yes'],
+	on_finish: function(data){
+		localSave = jsPsych.data.get().last(1).values()[0].response;
+	}
+};
+
+//Push components to experiment timeline
+ //from bds_adaptive.js
+timeline.push(bds_adaptive);
+timeline.push(save_data);
